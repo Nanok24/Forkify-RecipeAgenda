826b52b78a596c3c2ead2f82081472f8489e8cc2
Nanok24 <ionmihneaandrei@gmail.com>
2022-04-13 18:43:43 +0300

    Refactored code for MVC Architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,11 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
+const copyrightContainer = document.querySelector('.copyright');
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +18,29 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    // Get hash id
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+controlRecipes();
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
+
+//
