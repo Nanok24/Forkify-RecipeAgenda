c9d61f48c74de88528cf195a0ad05f2c8af13b76
Nanok24 <ionmihneaandrei@gmail.com>
2022-04-21 02:27:39 +0300

    Implemented the search functionality and also added pagination for the search results

--- src/js/model.js
+++ src/js/model.js
@@ -1,17 +1,23 @@
+import { async } from 'regenerator-runtime';
+import { API_URL, RES_PER_PAGE } from './config';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
     // Rendering recipe
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcae5'
-    );
-    const data = await res.json();
+    const data = await getJSON(`${API_URL}${id}`);
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcae5'
 
     const { recipe } = data.data;
     state.recipe = {
@@ -24,8 +30,38 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    //Error handling
+    console.error(`${err} 💥💥💥`);
+    throw err;
   }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.error(`${err} 💥💥💥`);
+    throw err;
+  }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
 };
